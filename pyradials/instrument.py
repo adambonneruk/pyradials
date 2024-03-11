ca3775fc6b5bda7d3b73eccd6fcf730af369af4d
adambonneruk <adam@bonner.uk>
2024-03-11 01:07:33 +0000

    read from gsi to setups and named tuples done

--- pyradials/instrument.py
+++ pyradials/instrument.py
@@ -1,205 +1,20 @@
-'''opens instrument file and parses them into a tidy dict'''
-import logging, sys, pathlib, re, json
-from textwrap import wrap
-from datetime import datetime
-from calc import mm_to_m, dms_to_decimal
+'''opens instrument file and parses them into a tidy list of setups and measurements'''
+import pathlib
+from leica import gsi
 
 def filename_details(fn: str):
-	''' returns lowercase stem and ext for a given filepath '''
+	''' returns stem and ext for a given filepath '''
 	stem: str = str(pathlib.Path(fn).stem)
 	suffix: str = str(pathlib.Path(fn).suffix)
 
-	return stem.lower(), suffix.lower()
+	return stem, suffix
 
-def leica_gsi_file_to_dict(full_fn: str, gsi_bit_depth: int = 16):
-	'''open a .gsi file and load into dictionary, optionally dump json of raw data'''
-	source = {}  # Create Dictionary for this .GSI Source
-	source['filename'] = '%s%s' % (filename_details(full_fn))
-	source['process_datetime'] = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-	source['format'] = 'Leica Geosystems %d-bit GSI (Geo Serial Interface)' % gsi_bit_depth
-
-	with open(full_fn, 'r') as leica_gsi:
-		leica_gsi_block = [text.rstrip() for text in leica_gsi.readlines()]
-
-	logging.debug(str(leica_gsi_block[0:2][0:80]) + "\n") # 80 chars of top 2 lines
-	parsed_leica_gsi_block = [dict() for _ in leica_gsi_block]
-
-	word_size: int = 0
-	trim_16bit_prefix: bool = False
-
-	match gsi_bit_depth:
-		case 8:
-			word_size: int = 12
-		case 16:
-			word_size: int = 24
-			trim_16bit_prefix = True
-			logging.debug('word size:%d, trim * prefix: %r' % (word_size, trim_16bit_prefix))
-		case _:
-			raise Exception('Unknown File Type: Leica .GSI Incorrect Bit-depth')
-
-	for i, leica_gsi_block in enumerate(leica_gsi_block, start=0):
-		leica_gsi_word = wrap(leica_gsi_block, word_size)
-
-		# trim the '*' from first word
-		leica_gsi_word[0] = leica_gsi_word[0][1:]
-
-		for word in leica_gsi_word:
-			match int(word[:2]):
-				case 11: # point number (includes block number)
-					parsed_leica_gsi_block[i]['block'] = re.sub(
-						r'^0+', r'', word[2:6])
-					parsed_leica_gsi_block[i]['shot'] = re.sub(
-						r'^0+', r'', word[7:])
-					parsed_leica_gsi_block[i]['type'] = 'point'
-				case 12: # instrument serial number
-					parsed_leica_gsi_block[i]['serial_number'] = re.sub(
-						r'^0+', r'', word[7:])
-					source['instrument_serial_number'] = parsed_leica_gsi_block[i]['serial_number']
-				case 18: # time format 1: YYSSsss
-					year = "20" + word[-8:-6]
-					sec = word[-6:-4]
-					# ms = word[-4:-1] # ms seems to always be .000
-				case 19: # time format 2: MMDDHHmm
-					month = word[-8:-6]
-					day = word[-6:-4]
-					hour = word[-4:-2]
-					min = word[-2:]
-				case 21: # horizontal circle (Hz)
-					parsed_leica_gsi_block[i]['Hz'] = int(
-						re.sub(r'^0+', r'', word[7:]))
-				case 22: # vertical angle (V)
-					parsed_leica_gsi_block[i]['V'] = int(
-						re.sub(r'^0+', r'', word[7:]))
-				case 31: # slope distance (SD)
-					parsed_leica_gsi_block[i]['Sd'] = int(
-						re.sub(r'^0+', r'', word[7:]))
-				case 32: # flat horizontal distance
-					parsed_leica_gsi_block[i]['flat_h_dist'] = int(re.sub(r'^0+', r'', word[7:]))
-				case 41: # code number (includes block number)
-					parsed_leica_gsi_block[i]['block'] = re.sub(
-						r'^0+', r'', word[2:6])
-					parsed_leica_gsi_block[i]['code'] = re.sub(
-						r'^0+', r'', word[7:])
-					parsed_leica_gsi_block[i]['type'] = 'code'
-				case 43:
-					parsed_leica_gsi_block[i]['shot'] = re.sub(
-						r'^0+', r'', word[7:])
-				case 45:
-					parsed_leica_gsi_block[i]['shot'] = re.sub(
-						r'^0+', r'', word[7:])
-				case 79: # Attrib 8
-					parsed_leica_gsi_block[i]['backsight'] = re.sub(
-						r'^0+', r'', word[7:])
-				case 81: # Easting (target)
-					zero_safe_word = 0
-					if re.sub(r'^0+', r'', word[7:]) != '':
-						zero_safe_word = int(re.sub(r'^0+', r'', word[7:]))
-					parsed_leica_gsi_block[i]['Et'] = zero_safe_word
-				case 82: # Northing (target)
-					zero_safe_word = 0
-					if re.sub(r'^0+', r'', word[7:]) != '':
-						zero_safe_word = int(re.sub(r'^0+', r'', word[7:]))
-					parsed_leica_gsi_block[i]['Nt'] = zero_safe_word
-				case 83: # Elevation (target)
-					zero_safe_word = 0
-					if re.sub(r'^0+', r'', word[7:]) != '':
-						zero_safe_word = int(re.sub(r'^0+', r'', word[7:]))
-					parsed_leica_gsi_block[i]['Et'] = zero_safe_word
-				case 84: # Station Easting (Eo)
-					parsed_leica_gsi_block[i]['type'] = 'setup' # Make this specific block a setup
-					zero_safe_word = 0
-					if re.sub(r'^0+', r'', word[7:]) != '':
-						zero_safe_word = int(re.sub(r'^0+', r'', word[7:]))
-					parsed_leica_gsi_block[i]['Eo'] = zero_safe_word
-				case 85: # Station Northing (No)
-					zero_safe_word = 0
-					if re.sub(r'^0+', r'', word[7:]) != '':
-						zero_safe_word = int(re.sub(r'^0+', r'', word[7:]))
-					parsed_leica_gsi_block[i]['No'] = zero_safe_word
-				case 86: # Station Elevation (Ho)
-					zero_safe_word = 0
-					if re.sub(r'^0+', r'', word[7:]) != '':
-						zero_safe_word = int(re.sub(r'^0+', r'', word[7:]))
-					parsed_leica_gsi_block[i]['Ho'] = zero_safe_word
-				case 87: # Reflector height (above ground)
-					zero_safe_word = 0
-					if re.sub(r'^0+', r'', word[7:]) != '':
-						zero_safe_word = int(re.sub(r'^0+', r'', word[7:]))
-					parsed_leica_gsi_block[i]['Rh'] = zero_safe_word
-				case 88: # Instrument height (above ground)
-					parsed_leica_gsi_block[i]['Ih'] = int(
-						re.sub(r'^0+', r'', word[7:]))
-		try:
-			parsed_leica_gsi_block[i]['date_time'] = (
-				str(year) + "-" + str(month) + "-" + str(day)
-				+ " " + str(hour) + ":" + str(min) + ":" + str(sec)
-			)
-			source['capture_datetime'] = parsed_leica_gsi_block[i]['date_time']
-		except:
-			logging.debug("this particular block doesn't have datetime info")
-
-		# store the block-word array as raw
-		parsed_leica_gsi_block[i]['raw'] = leica_gsi_word
-
-	return parsed_leica_gsi_block
-
-def leica_dict_to_radials(leica_dict):
-	# the leica_gsi is now parsed_leica_gsi, lets grab the relevant data for source{}
-	radials = []
-	for parsed_block_value in leica_dict:
-		match parsed_block_value['type']:
-			case 'setup':
-				# put a dictionary in the observation array for a setup, then populate with setup details
-				source['setup'].append(dict())
-				source['setup'][-1]['station'] = parsed_block_value.get('shot')
-				source['setup'][-1]['backsight'] = parsed_block_value.get('backsight')
-				source['setup'][-1]['instrument_height'] = mm_to_m(parsed_block_value.get('Ih'))
-				source['setup'][-1]['target_height'] = mm_to_m(parsed_block_value.get('Rh'))
-				source['setup'][-1]['station_easting'] = mm_to_m(parsed_block_value.get('Eo'))
-				source['setup'][-1]['station_northing'] = mm_to_m(parsed_block_value.get('No'))
-				source['setup'][-1]['station_elevation'] = mm_to_m(parsed_block_value.get('Ho'))
-				source['setup'][-1]['shot'] = []
-
-			case 'code':
-				most_recent_code: str = parsed_block_value.get('code')
-				if parsed_block_value.get('shot') != '':
-					most_recent_comma_code: str = parsed_block_value.get(
-						'shot')
-				else:
-					most_recent_comma_code = None
-
-			case 'point':
-				logging.debug(parsed_block_value)
-				# derive the full comma-code
-				if most_recent_comma_code:
-					current_code = str(most_recent_code + "," + most_recent_comma_code)
-				else:
-					current_code = most_recent_code
-
-				logging.debug(parsed_block_value.get('V'))
-				logging.debug(parsed_block_value.get('shot'))
-				logging.debug(current_code)
-				logging.debug(parsed_block_value.get('Sd'))
-
-				# built the tuple
-				source['setup'][-1]['shot'].append(tuple((
-					parsed_block_value.get('shot'),						# Shot = Point Number
-					current_code,										# Code = Codelist Code, Comma Code
-					mm_to_m(parsed_block_value.get('Sd')),				# Radius = Slope Distance
-					dms_to_decimal(str(parsed_block_value.get('V'))),	# Inclination = Vertical Angle
-					dms_to_decimal(str(parsed_block_value.get('Hz'))),	# Azimuth = Horizontal Angle
-					mm_to_m(parsed_block_value.get('Rh'))				# Target Height = Reflector Height
-				)))
-
-			case _:
-				raise Exception('Not a Setup, Code or Point')
-
-def instrument_file(full_fn: str, export_json: bool = False):
+def instrument_file_as_source(full_fn:str, debug_json_output:bool = False):
 	'''with a given fn, check compatibility/support and load the file into memory'''
 
 	stem, suffix = filename_details(full_fn)
 
-	match suffix:
+	match suffix.lower():
 		case ".gsi":  # Leica TPS 1100/1200 Series 8/16-bit Data
 			# open file, read first line to determine bit-depth
 			with open(full_fn, 'r') as leica_gsi:
@@ -208,13 +23,8 @@ def instrument_file(full_fn: str, export_json: bool = False):
 			# * in first position equals 16-bit
 			if first_line[0] == '*':
 				gsi_bit_depth = 16
-				leica_dict = leica_gsi_file_to_dict(full_fn, gsi_bit_depth)
-				#source = leica_dict_to_radials(leica_dict)
+				source = gsi(full_fn, gsi_bit_depth, debug_json_output)
 
-			# json export
-			if export_json:
-				with open(pathlib.Path(full_fn).with_suffix('.json'), "w") as write_file:
-					write_file.write(json.dumps(leica_dict) + "\n")
 			else:
 				gsi_bit_depth = 8
 				raise Exception('8-bit Leica .GSI Not Supported')
@@ -232,4 +42,8 @@ def instrument_file(full_fn: str, export_json: bool = False):
 			raise Exception('Unknown File Type')
 
 	# output the opened parsed instrument file
-	return leica_dict
+	return source
+
+if __name__ == "__main__":
+	source = instrument_file_as_source('data/SPORTRD1.GSI')
+	print(source)
---
+++ pyradials/leica.py
@@ -0,0 +1,244 @@
+import logging, json
+from collections import namedtuple
+from textwrap import wrap
+from calc import mm_to_m, dms_to_decimal
+
+'''
+General
+	11	Point number (includes block number)
+	12	Instrument serial number
+	13	Instrument type
+	18	Time format 1:
+	19	Time format 2:
+
+Angles
+	21	Horizontal circle (Hz)
+	22	Vertical angle (V)
+	25	Horizontal circle difference(Hz0-Hz)
+
+Distances
+	31	Slope distance
+	32	Horizontal distance
+	33	Height difference
+
+Codeblock.
+	41	Code number (includes block number)
+	42+	Information 1-8
+
+Distance (additional information)
+	51	Constants (ppm,mm)
+	52	Number of measurements
+	53	Standard Deviation
+	58	Signal strength
+	59	Reflector constant (1/10 mm) ppm
+
+Point coding
+	71	Point code
+	72+	Attrib. 1-8
+
+Coordinates
+	81	Easting (target)
+	82	Northing (target)
+	83	Elevation (target)
+	84	Station easting (Eo)
+	85	Station northing (No)
+	86	Station elevation (Ho)
+	87	Reflector height (above ground)
+	88	Instrument height (above ground)
+'''
+
+def strip_leading_zeros(input_str:str, field_type='int'):
+	if input_str == '0' * len(input_str):
+		return 0
+
+	# Strip leading zeros
+	stripped_str = input_str.lstrip('0')
+
+	# Try converting to integer
+	try:
+		result = int(stripped_str)
+	except ValueError:
+		# If conversion fails, return as string
+		result = stripped_str
+
+	return result if field_type == 'int' else str(result)
+
+def derive_date_time(w18:str, w19:str) -> str:
+	if w18 is not None and w19 is not None:
+		w18 = "0" + str(w18)
+		w19 = "0" + str(w19)
+
+		year:str = "20" + w18[-8:-6]
+		month:str = w19[-8:-6]
+		day:str = w19[-6:-4]
+		hour:str = w19[-4:-2]
+		min:str = w19[-2:]
+		sec:str = w18[-6:-4]
+
+		return (str(year) + "-" + str(month) + "-" + str(day) + " " + str(hour) + ":" + str(min) + ":" + str(sec))
+
+def gsi_to_blocks_list(fn:str, bit_depth:int = 16, debug_json_output = False) -> list:
+	'''takes a gsi filename and returns a list of tuples with the setups, codes, and measurements'''
+	# Set word_size based on bit_depth
+	match bit_depth:
+		case 8:
+			word_size:int = 12
+			trim_16bit_prefix = False
+			logging.debug('word size:%d, trim * prefix: %r' % (word_size, trim_16bit_prefix))
+
+		case 16:
+			word_size:int = 24
+			trim_16bit_prefix = True
+			logging.debug('word size:%d, trim * prefix: %r' % (word_size, trim_16bit_prefix))
+
+		case _:
+			raise Exception('Unknown File Type: Leica .GSI Incorrect Bit-depth')
+
+	# Open and read the file
+	with open(fn, 'r') as gsi_file:
+		gsi_file_reader = [text.rstrip() for text in gsi_file.readlines()]
+
+	list_of_blocks = []
+	# Named Tuples
+	Setup = namedtuple("Setup","type station w84 w85 w86 w88 w79 raw")
+	Code = namedtuple("Code","type code w45 w48 raw")
+	Measurement = namedtuple("Measurement","type point_id w21 w22 w31 w87 date_time raw")
+
+	for row in gsi_file_reader:
+		words = wrap(row, word_size)
+
+		if trim_16bit_prefix:
+			words[0] = words[0][1:]
+
+		intermediate_dict = {}
+
+		for word in words:
+			intermediate_dict[word[:2]] = strip_leading_zeros(word[7:])
+
+		if intermediate_dict.get('84') is not None:
+			logging.debug("this block is a setup")
+			setup = Setup("setup",
+				intermediate_dict.get('11'),
+				intermediate_dict.get('84'),
+				intermediate_dict.get('85'),
+				intermediate_dict.get('86'),
+				intermediate_dict.get('88'),
+				intermediate_dict.get('79'),
+				words,
+			)
+			list_of_blocks.append(setup)
+
+		elif intermediate_dict.get('41') is not None:
+			logging.debug("this block is a code")
+			code = Code("code",
+				intermediate_dict.get('41'),
+				intermediate_dict.get('45'),
+				intermediate_dict.get('48'),
+				words,
+			)
+			list_of_blocks.append(code)
+
+		else:
+			logging.debug("this block is a measurement")
+			measurement = Measurement("measurement",
+				intermediate_dict.get('11'),
+				intermediate_dict.get('21'),
+				intermediate_dict.get('22'),
+				intermediate_dict.get('31'),
+				intermediate_dict.get('87'),
+				derive_date_time(intermediate_dict.get('18'),intermediate_dict.get('19')),
+				words,
+			)
+			list_of_blocks.append(measurement)
+
+	if debug_json_output:
+		with open('debug/processed_gsi.json', "w") as write_file:
+			write_file.write(json.dumps(list_of_blocks) + "\n")
+
+	return list_of_blocks
+
+def check_integrity_of_setup(setup_block,ro_code_block,ro_measurement) -> None:
+
+	logging.debug(setup_block)
+
+	if setup_block.type != "setup":
+		raise Exception('gsi does not start with a setup')
+	if ro_code_block.type != "code":
+		raise Exception('gsi does not follow setup with a RO')
+	if ro_code_block.code != "RO":
+		raise Exception('code is not RO following setup')
+	if ro_measurement.type != "measurement":
+		raise Exception('ro is missing the measurement')
+
+def combine_strings(str1:str , str2:str) -> str:
+	if str1 is not None and str2 is not None:
+		return f"{str1},{str2}"
+	elif str1 is not None:
+		return str1
+	elif str2 is not None:
+		return str2
+	else:
+		return None
+
+def reduce_and_code_measurements(gsi_blocks:list , debug_json_output:bool = False) -> dict:
+
+	check_integrity_of_setup(gsi_blocks[0],gsi_blocks[1],gsi_blocks[2])
+	setups_with_coded_measurements = []
+	Coded_Measurement = namedtuple("Coded_Measurement", "point_id code attrib hz vt sd th")
+
+	recent_code:str = None
+	recent_attrib:str = None
+
+	for i, block in enumerate(gsi_blocks, start=0):
+		match block.type:
+			case "setup":
+				check_integrity_of_setup(gsi_blocks[i],gsi_blocks[i+1],gsi_blocks[i+2])
+				setups_with_coded_measurements.append(dict())
+				setups_with_coded_measurements[-1]['station'] = block.station
+				setups_with_coded_measurements[-1]['easting'] = mm_to_m(block.w84)
+				setups_with_coded_measurements[-1]['northing'] = mm_to_m(block.w85)
+				setups_with_coded_measurements[-1]['elevation'] = mm_to_m(block.w86)
+				setups_with_coded_measurements[-1]['height'] = mm_to_m(block.w88)
+				setups_with_coded_measurements[-1]['backsight'] = block.w79
+				setups_with_coded_measurements[-1]['date_time'] = None
+				setups_with_coded_measurements[-1]['coded_measurements'] = []
+
+			case "code":
+				recent_code = block.code
+				recent_attrib = combine_strings(block.w45,block.w48)
+
+			case "measurement":
+				coded_measurement = Coded_Measurement(
+					block.point_id,
+					recent_code,
+					recent_attrib,
+					dms_to_decimal(str(block.w21)),
+					dms_to_decimal(str(block.w22)),
+					mm_to_m(block.w31),
+					mm_to_m(block.w87)
+				)
+
+				if block.date_time is not None:
+					setups_with_coded_measurements[-1]['date_time'] = block.date_time
+
+				setups_with_coded_measurements[-1]['coded_measurements'].append(coded_measurement)
+
+			case _: raise Exception('block isn\'t a code, measurement or setup')
+
+	if debug_json_output:
+		with open('debug/reduce_measurements.json', "w") as write_file:
+			write_file.write(json.dumps(setups_with_coded_measurements) + "\n")
+
+	return setups_with_coded_measurements
+
+def gsi(fn:str, bit_depth:int, debug_json_output:bool = False) -> list:
+	gsi_blocks = gsi_to_blocks_list(fn,bit_depth,debug_json_output)
+	data = reduce_and_code_measurements(gsi_blocks,True)
+
+	return data
+
+if __name__ == "__main__":
+	#data = main('data/OFFICE.GSI',16,True)
+	data = gsi('data/SPORTRD1.GSI',16,True)
+	print(data)
+
